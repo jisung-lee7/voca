479495871502254322379905c8b7188c2b7d8345
Jisung Lee <jskurt9352@gmail.com>
2024-06-28 17:07:28 +0900

    add EmptyPage

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
 import "./App.css";
 import Day from "./component/Day";
 import DayList from "./component/DayList";
+import EmptyPage from "./component/EmptyPage";
 import Header from "./component/Header";
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
         <Routes>
           <Route path="/" element={<DayList />} />
           <Route path="/day/:day" element={<Day />} />
+          <Route path="*" element={<EmptyPage />} />
         </Routes>
       </Router>
     </div>
